b38e23a5e89a83a1bae0e01b092a4cd2fb8eff43
Ahmad Efriyanto <a.efriyanto@gmail.com>
2022-04-01 10:26:13 +0700

    main : basic code of news app

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:news_app/article.dart';
+import 'package:flutter/material.dart';
+import 'package:news_app/detail_page.dart';
+import 'package:news_app/widget/list_page.dart';
+import 'package:news_app/style.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(elevation: 0),
+        textTheme: myTextTheme,
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+              primary: primaryColor,
+              onPrimary: Colors.black,
+              secondary: secondaryColor,
+            ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: secondaryColor,
+            onPrimary: Colors.white,
+            textStyle: TextStyle(),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(0),
+              ),
+            ),
+          ),
+        ),
+      ),
+      initialRoute: NewsListPage.routeName,
+      routes: {
+        NewsListPage.routeName: (context) => NewsListPage(),
+        ArticleDetailPage.routeName: (context) => ArticleDetailPage(
+              article: ModalRoute.of(context)?.settings.arguments as Article,
+            ),
+        ArticleWebView.routeName: (context) => ArticleWebView(
+              url: ModalRoute.of(context)?.settings.arguments as String,
+            ),
+      },
+    );
+  }
+}
